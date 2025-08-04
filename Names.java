25ab05eb0a84a55a4c36a5cefbf6b29247c08bd5
Studentsofteng <tenzin.tsultrim@students.mq.edu.au>
2025-08-04 14:25:50 +1000

    Update Names.java
    Tenzin Name function

--- Names.java
+++ Names.java
@@ -3,11 +3,16 @@ public class Names {
 		System.out.println("This group consists of:");
 		// Put your signature function on the below line!
 		PrintHarrisSig();
+		PrintTenzinSig();
 		// Put your signature function on the above line!
 	}
 	
 	public static void PrintHarrisSig() {
 		System.out.println(".--.     /\\                ____");
 		System.out.println("'--'    /__\\    (^._.^)~ <(o.o )>");
+	}
+
+	public static void PrintTenzinSig() {
+		System.out.println("null");
 	}
 }
